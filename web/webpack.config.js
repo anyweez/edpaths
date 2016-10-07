361c424072260017a47d661fcfb06541e78632ba
Luke Segars <luke@theironyard.com>
2016-10-07 16:36:06 -0400

    Respects NODE_ENV=production.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -24,4 +24,9 @@ module.exports = [{
       }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+  ],
 }];
